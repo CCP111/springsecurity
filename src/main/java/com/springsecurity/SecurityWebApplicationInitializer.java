f66c57212a1711176cd058221b0c6883c6def070
root <root@localhost.localdomain>
2020-12-05 00:26:36 +0700

    first commit

---
+++ src/main/java/com/springsecurity/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.springsecurity;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+
+}
